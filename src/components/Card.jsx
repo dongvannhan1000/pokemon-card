dac3e605d8414d2a6c4fe1526a01030b97227d85
Nhan Dong <nhandong0205@gmail.com>
2024-08-16 23:24:51 +0700

    Add card

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import { capitalize } from "./utils";
+import '../styles/Card.css';
+
+export default function Card({pokemon, handleClick, isFlipped}) {
+    const types = pokemon.types.map(type => type.type.name);
+
+    return (
+        <div
+            onClick={() => handleClick(pokemon)}
+            className={`card ${isFlipped ? 'flipped' : ''}`}>
+            <div className="card-inner">
+                <div className="card-face front">
+                    <div className="pokemon-image">
+                        <img src={pokemon.images.other['official-artwork'].front_default} alt={pokemon.name} />
+                    </div>
+                    <div className="pokemon-info">
+                        <h5>{capitalize(pokemon.name)}</h5>
+                        <div className="pokemon-types">
+                            {types.map(type => (
+                                <span key={type} className={`type-label ${type}`}>{capitalize(type)}</span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="card-face back"></div>
+            </div>
+        </div>
+    );
+}
